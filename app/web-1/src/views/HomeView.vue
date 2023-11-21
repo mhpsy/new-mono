f7dfd5687e46c66694c4dd56be02e6ff6d464891
A pessimistic guide to living <object@tanyu.mobi>
2023-11-21 20:36:19 +0800

    init monorepo

---
+++ app/web-1/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ElButton } from 'element-plus';
+import {add} from '@m/utils';
+
+console.log(add(1, 2));
+
+</script>
+
+<template>
+  <main>
+    <el-button :type="'primary'" circle>123123</el-button>
+  </main>
+</template>
